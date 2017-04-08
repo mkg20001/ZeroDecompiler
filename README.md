00d20ca812d30b349a97a0a741278df293ff192f
mkg20001 <mkg20001@gmail.com>
2017-04-08 15:35:02 +0200

    Add README - Beautify - Add warnings - Add colors

---
+++ README.md
@@ -0,0 +1,10 @@
+# ZeroDecompiler
+Decompile a ZeroNet Zite
+
+Usage: `zero-decompiler <ZiteRoot> <OutDir> [-Warn]`
+
+Run with `-Warn` to see warnings generated when decompiling the coffeescript
+
+Uses js2coffee to decompile the javascript.
+
+The resulting .coffee files may look different.
--- cli.js
+++ cli.js
@@ -1,42 +1,79 @@
 //$0: <zite root> <out dir>
-const path=require("path")
-const fs=require("fs")
-const mkdirp=require("mkdirp")
-const js2cof=require("js2coffee")
+const path = require("path")
+const fs = require("fs")
+const mkdirp = require("mkdirp")
+const js2cof = require("js2coffee")
 
-const splitter=require(__dirname+"/lib/fileSplit.js")
+function repeat(s, n) {
+  var r = "";
+  for (var a = 0; a < n; a++) r += s;
+  return r;
+}
+const preview = function Preview(lines, start, end) {
+  const prev = lines[start.line - 2]
+  const cur = lines[start.line - 1]
+  const after = lines[start.line]
+  console.log(prev)
+  console.log(cur.bold)
+  if (end)
+    if (end.line != start.line) end = null;
+  if (end) {
+    console.log(repeat(" ", start.column - 1), repeat("~", end.column - start.column).red.bold)
+  } else {
+    console.log(repeat(" ", start.column - 1), "^".red.bold)
+  }
+  console.log(after)
+}
+require("colors")
 
-const root=path.resolve(process.argv[2])
-const out=path.resolve(process.argv[3])
-const files=[path.join(root,"js","all.js"),path.join(root,"css","all.css")]
+const splitter = require(__dirname + "/lib/fileSplit.js")
+
+const root = path.resolve(process.argv[2])
+const out = path.resolve(process.argv[3])
+const files = [path.join(root, "js", "all.js"), path.join(root, "css", "all.css")]
 mkdirp.sync(out);
 
-files.concat([root,out]).map(p => {if (!fs.existsSync(p)) throw new Error("ENOTFOUND: "+p)})
+files.concat([root, out]).map(p => {
+  if (!fs.existsSync(p)) throw new Error("ENOTFOUND: " + p)
+})
 
 files.map(f => {
-  const content=fs.readFileSync(f).toString()
-  const inside=splitter(content.split("\n")).map(file => {
-    if (file.type=="coffee") {
+  const content = fs.readFileSync(f).toString()
+  const inside = splitter(content.split("\n")).map(file => {
+    if (file.type == "coffee") {
       try {
-        file.orig=file.content
-        file.coffee=js2cof.build(file.content)
-        file.content=file.coffee.code
-      } catch(e) {
+        file.orig = file.content
+        file.coffee = js2cof.build(file.content)
+        file.content = file.coffee.code
+        if (process.argv[4] && process.argv[4].toLowerCase().startsWith("-w")) file.coffee.warnings.forEach(warn => {
+          const show = ["[WARN]".yellow.bold]
+          show.push(file.path.white.bold, "@".yellow.bold)
+          if (warn.end) {
+            show.push(warn.start.line + ":" + warn.start.column + " -> " + warn.end.line + ":" + warn.end.column)
+          } else if (warn.start) {
+            show.push(warn.start.line + ":" + warn.start.column)
+          } else {
+            show.push("?".grey)
+          }
+          show.push(warn.description)
+          console.log.apply(console, show)
+          if (warn.start) preview(file.lines, warn.start, warn.end)
+          console.log("")
+        })
+      } catch (e) {
         if (e.start) {
-          console.error(e.message.replace(/^input\.js/,file.path.replace(/\.coffee$/,".js")))
-          //console.error(e.sourcePreview)
-          //console.error("[E] %s %s:%s -> %s:%s | %s",file.path,file.start.line,file.start.column,file.end.line,file.end.column)
+          console.error(e.message.replace(/^input\.js/, file.path.replace(/\.coffee$/, ".js")))
         } else {
-          console.error("[COFFEE ERROR]",e)
+          console.error("[JS2COFFEE ERROR]".red.bold, e)
         }
       }
     }
-    file.out=path.join(out,file.path)
+    file.out = path.join(out, file.path)
     return file
   })
   inside.map(file => {
-    console.log("%s => %s",file.path,file.out)
+    console.log("[WRITE]".blue.bold + " %s => %s".white.bold, file.path, file.out)
     mkdirp.sync(path.dirname(file.out))
-    fs.writeFileSync(file.out,new Buffer(file.content))
+    fs.writeFileSync(file.out, new Buffer(file.content))
   })
 })
